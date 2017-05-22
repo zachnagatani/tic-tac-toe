8cc3d1fca6cbda6c25acdfd7084e1c5185aed55c
Zachariah Nagatani <zachnagatani@gmail.com>
2017-05-22 16:32:47 -0700

    Combine reducers and change site title

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import changePlayerReducer from './changePlayer';
+import ownedSquaresReducer from './ownedSquares';
+import turnsReducer from './turns';
+import winStatusReducer from './winStatus';
 
 const chingChongCha = combineReducers({
-    changePlayerReducer
+    changePlayerReducer,
+    ownedSquaresReducer,
+    turnsReducer,
+    winStatusReducer
 });
 
 export default chingChongCha;
