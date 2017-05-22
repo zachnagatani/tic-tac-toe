fa3af2c12461942d9d0454bfcf78ffeecf972998
Zachariah Nagatani <zachnagatani@gmail.com>
2017-05-22 14:40:43 -0700

    FIX: Change Player reducer should return a simple string, not a whole object

--- src/reducers/changePlayer.js
+++ src/reducers/changePlayer.js
@@ -1,11 +1,9 @@
-import * as types from './../actions/actionTypes';
+import * as types from '../actions/actionTypes';
 
-export default function changePlayerReducer(state = { currentPlayer: 'x' }, action) {
+export default function changePlayerReducer(state = 'x', action) {
     switch (action) {
         case types.CHANGE_PLAYER:
-            return {
-                currentPlayer: state.currentPlayer === 'x' ? 'o' : 'x'
-            };
+            return state.currentPlayer === 'x' ? 'o' : 'x'
         default:
             return state;
     }
--- src/reducers/changePlayer.test.js
+++ src/reducers/changePlayer.test.js
@@ -1,13 +1,13 @@
 import changePlayerReducer from './changePlayer';
-import * as types from './../actions/actionTypes';
+import * as types from '../actions/actionTypes';
 
 describe('Change players reducer', () => {
     it('should return the intial state', () => {
-        expect(changePlayerReducer(undefined, {})).toEqual({ currentPlayer: 'x' });
+        expect(changePlayerReducer(undefined, {})).toEqual('x');
     });
 
     it('should handle CHANGE_PLAYER', () => {
         expect(
-            changePlayerReducer({ currentPlayer: 'o' }, types.CHANGE_PLAYER)).toEqual({ currentPlayer: 'x' });
+            changePlayerReducer({ currentPlayer: 'o' }, types.CHANGE_PLAYER)).toEqual('x');
     });
 });
